aa9fbfe3d06c73cc45b17320b28f772ac423f480
zhoujingya <jing.zhou@terapines.com>
2023-12-26 14:30:46 +0800

    Add dialect definition

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/bash
 
+LLVM_TOOL_PATH=$HOME/tools/clang16
+export PATH=$LLVM_TOOL_PATH/bin:$PATH
+
+# Building toy dialect
+mlir-tblgen -gen-op-decls dialect/Ops.td -I$LLVM_TOOL_PATH/include -o dialect/Ops.h.inc
+mlir-tblgen -gen-op-defs dialect/Ops.td -I$LLVM_TOOL_PATH/include -o dialect/Ops.cpp.inc
+mlir-tblgen -gen-dialect-decls dialect/Ops.td -I$LLVM_TOOL_PATH/include -o dialect/Dialect.h.inc
+mlir-tblgen -gen-dialect-defs dialect/Ops.td -I$LLVM_TOOL_PATH/include -o dialect/Dialect.cpp.inc
+
 cmake -B build \
     -S . \
     -G Ninja \
