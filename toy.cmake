556e7664e201f9963eb76debf1c774d0a549dec9
zhoujing <jing.zhou@terapines.com>
2024-02-28 17:54:59 +0800

    Add missing cmake config file

--- toy.cmake
+++ toy.cmake
@@ -4,11 +4,14 @@ set(LLVM_TARGET_DEFINITIONS toy/ToyCombine.td)
 mlir_tablegen(ToyCombine.inc -gen-rewriters)
 add_public_tablegen_target(ToyCombineIncGen)
 
+get_property(dialect_libs GLOBAL PROPERTY MLIR_DIALECT_LIBS)
+get_property(extension_libs GLOBAL PROPERTY MLIR_EXTENSION_LIBS)
 add_mlir_library(toy
     toy/AST.cpp
     toy/Dialect.cpp
     toy/MLIRGen.cpp
     toy/ToyCombine.cpp
+    toy/toyToAffineLoops.cpp
 
     DEPENDS
     ToyOpsIncGen
@@ -16,5 +19,5 @@ add_mlir_library(toy
 )
 
 add_executable(mlir-toy toy/toy.cpp)
-target_link_libraries(mlir-toy toy)
+target_link_libraries(mlir-toy toy ${dialect_libs} ${extension_libs})
 include_directories(${CMAKE_CURRENT_SOURCE_DIR}/include)
